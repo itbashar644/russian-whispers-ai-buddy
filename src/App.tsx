15876a38dc707d4d2cc4a9b0b7720948388bf393
itbashar644 <halafbashar@gmail.com>
2025-05-26 22:23:41 +0000

    chore: drop SPA /product route, use static pages

--- src/App.tsx
+++ src/App.tsx
@@ -62,8 +62,8 @@ function App() {
                 <YandexMetrika />
                 <Routes>
                   <Route path="/" element={<Index />} />
-                  <Route path="/product/:id" element={<ProductDetail />} />
-                  <Route path="/catalog" element={<Catalog />} />
+                  {/*
+                  <Route path="/catalog" element={<Catalog />} />*/}
                   <Route path="/cart" element={<Cart />} />
                   <Route path="/about" element={<About />} />
                   <Route path="/contacts" element={<Contacts />} />
